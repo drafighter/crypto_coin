5a5101b90b251c5eb66bf5c31636849c092eb390
jdragon <ldap01@naver.com>
2020-11-10 23:52:16 +0900

    MongoDB 구현(HO)_201110

--- autotrading/db/mongodb/mongodb_handler.py
+++ autotrading/db/mongodb/mongodb_handler.py
@@ -1,6 +1,6 @@
 import configparser
 
-from pymongo import MongoClient
+from pymongo import MongoClient, CursorType
 
 from autotrading.db.base_handler import DBHandler
 
@@ -31,15 +31,110 @@ class MongoDBHandler(DBHandler):
         self.db_config = {}
         self.db_config["local_ip"] = config["MONGODB"]["local_ip"]
         self.db_config["port"] = config["MONGODB"]["port"]
-        self.db_config["remote_host"] = config["MONGODB"]["remote_host"]
-        self.db_config["remote_port"] = config["MONGODB"]["remote_port"]
-        self.db_config["user"] = config["MONGODB"]["user"]
-        self.db_config["password"] = config["MONGODB"]["password"]
+        # self.db_config["remote_host"] = config["MONGODB"]["remote_host"]
+        # self.db_config["remote_port"] = config["MONGODB"]["remote_port"]
+        # self.db_config["user"] = config["MONGODB"]["user"]
+        # self.db_config["password"] = config["MONGODB"]["password"]
 
         if mode == "remote":
             self._client = MongoClient("mongodb://{user}:{password}@{remote_host}:{port}".format(**self.db_config))
         elif mode == "local":
-            self._client = MongoClient("mongodb://{user}:{password}@{local_ip}:{port}".format(**self.db_config))
+            self._client = MongoClient("mongodb://{local_ip}:{port}".format(**self.db_config))
 
         self._db = self._client[db_name]
         self._collection = self._db[collection_name]
+
+    def set_db_collection(self, db_name=None, collection_name=None):
+        """
+        MongoDB 에서 작업하려는 database 와 collection 을 변경할때 사용
+        :param db_name: database 에 해당하는 이름
+        :param collection_name: database 에 속하는 collection 이름
+        :return: None
+        """
+        if db_name is None:
+            raise Exception("Need to dbname name")
+
+        self._db = self._client[db_name]
+        if collection_name is not None:
+            self._collection = self._db[collection_name]
+
+    def get_current_db_name(self):
+        """
+        현재 MongoDB 에서 작업 중인 database 의 이름을 반환합니다.
+
+        :raises:
+            self._db.name : 현재 사용중인 database 이름을 반환
+        """
+        return self._db.name
+
+    def get_current_collection_name(self):
+        """
+        현재 MongoDB 에서 작업 중인 collection 의 이름을 반환합니다.
+
+        :raises:
+            self._collection.name : 현재 사용중인 collection 이름을 반환
+        """
+        return self._collection.name
+
+    def insert_item(self, data, db_name=None, collection_name=None):
+        """
+        MongoDB 에 하나의 document 를 입력하기 위한 메소드
+        :param data:
+        :param db_name:
+        :param collection_name:
+        :return: inserted_id : 입력 완료된 문서의 ObjectId 반환
+        """
+        if db_name is not None:
+            self._db = self._client[db_name]
+        if collection_name is not None:
+            self._collection = self._db[collection_name]
+        return self._collection.insert_one(data).inserted_id
+
+    def insert_items(self, datas, db_name=None, collection_name=None):
+        """
+        MongoDB에 다수의 document 를 입력하기 위한 메소드
+        :param datas:
+        :param db_name:
+        :param collection_name:
+        :return: inserted_ids : 입력 완료된 문서의 objectId list 를 반환
+        """
+        if db_name is not None:
+            self._db = self._client[db_name]
+        if collection_name is not None:
+            self._collection = self._db[collection_name]
+        return self._collection.insert_many(datas).inserted_ids
+
+    def find_items(self, condition=None, db_name=None, collection_name=None):
+        """
+        MongoDB 에서 다수의 document 를 검색하기 위한 메소드
+        :param condition: 검색조건을 dictionary 형태로 입력 받음
+        :param db_name:
+        :param collection_name:
+        :return: Cursor 를 반환
+        """
+        if condition is None:
+            condition = {}  # dictionary
+        if db_name is not None:
+            self._db = self._client[db_name]
+        if collection_name is not None:
+            self._collection = self._db[collection_name]
+        return self._collection.find(condition, no_cursor_timeout=True, cursor_type=CursorType.EXHAUST)
+
+    def find_item(self, condition, db_name=None, collection_name=None):
+        """
+        MongoDB 에서 하나의 document 를 검색하기 위한 메소드
+        :param condition: 검색조건을 dictionary 형태로 입력 받음
+        :param db_name:
+        :param collection_name:
+        :return: 검색된 문서가 있으면 문서의 내용을 반환
+        """
+        if condition is None:
+            condition = {}  # dictionary
+        if db_name is not None:
+            self._db = self._client[db_name]
+        if collection_name is not None:
+            self._collection = self._db[collection_name]
+        return self._collection.find_one(condition)
+
+
+
--- tests/test_mongodb_handler.py
+++ tests/test_mongodb_handler.py
@@ -7,7 +7,7 @@ from autotrading.db.mongodb.mongodb_handler import MongoDBHandler
 class MongoDBHandlerTestCase(unittest.TestCase):
     def setUp(self):
         self.mongodb = MongoDBHandler("local", "coiner", "price_info")
-        self.mongodb.delete_items({})
+        # self.mongodb.delete_items({})
         docs = [
             {"currency": "btc", "price": 10000},
             {"currency": "eth", "price": 1000},
@@ -16,7 +16,45 @@ class MongoDBHandlerTestCase(unittest.TestCase):
             {"currency": "eth", "price": 2000},
             {"currency": "xrp", "price": 200}
         ]
-        self.mongodb.insert_items(docs)
+        # self.mongodb.insert_items(docs)
+
+    # def test_set_db_collection(self):
+    #     """
+    #     test set_db
+    #     :return:
+    #     """
+    #     print(inspect.stack()[0][3])
+    #     self.mongodb.set_db_collection("trader", "trade_status")
+    #     self.assertEqual(self.mongodb.get_current_db_name(), "trader")
+    #     self.assertEqual(self.mongodb.get_current_collection_name(), "trade_status")
+
+    # def test_insert_item(self):
+    #     print(inspect.stack()[0][3])
+    #     doc = {"item": "item0", "name": "test_insert_item"}
+    #     id = self.mongodb.insert_item(doc)
+    #     assert id
+    #     print(id)
+    #
+    # def test_insert_items(self):
+    #     print(inspect.stack()[0][3])
+    #     docs = [{"item": "item1", "name": "test_insert_items"},
+    #             {"item": "item2", "name": "test_insert_items"}
+    #             ]
+    #     ids = self.mongodb.insert_items(docs)
+    #     assert ids
+    #     print(ids)
+
+    def test_find_item(self):
+        print(inspect.stack()[0][3])
+        doc = self.mongodb.find_item({"currency": "btc"})
+        print(doc)
+
+    def test_find_items(self):
+        print(inspect.stack()[0][3])
+        cursor = self.mongodb.find_items({"currency": "eth"})
+        assert cursor
+        for doc in cursor:
+            print(doc)
 
     def tearDown(self):
         pass
