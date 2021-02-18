051926b4771210a8376411b371521cf84fc9991d
jdragon <ldap01@naver.com>
2021-02-18 16:34:58 +0900

    strategy_study.py 부분 작성_210218_사무실

---
+++ api_test/strategy_study.py
@@ -0,0 +1,21 @@
+import pyupbit
+import urllib3
+import pprint
+
+# warning message 무시
+urllib3.disable_warnings()
+
+tickers = pyupbit.get_tickers('KRW')
+# pprint.pprint(tickers)
+
+for a in tickers:
+    if 'LINK' in a:
+        # print(f"Found it! {a}")
+        pass
+
+chart = pyupbit.get_ohlcv(ticker="KRW-OBSR")
+print(chart)
+# D-1 종가
+d_1_close = chart.iloc[-2]['close']
+print(d_1_close)
+
