88a9bd7678ed0a81e9d22960876b6a483d9580ee
jdragon <ldap01@naver.com>
2021-03-05 11:20:46 +0900

    pyqt_study.py 부분 작성_210305_사무실

--- api_test/pyqt_study.py
+++ api_test/pyqt_study.py
@@ -1,9 +1,16 @@
 import sys
+import pyupbit
+import urllib3
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
 
-form_class = uic.loadUiType("ui/mywindow.ui")[0]
+# warning message 무시
+urllib3.disable_warnings()
+
+# form_class = uic.loadUiType("ui/mywindow.ui")[0]
+form_class = uic.loadUiType("ui/upbit_sise.ui")[0]
 
 
 class MyWindow(QMainWindow, form_class):
@@ -23,12 +30,23 @@ class MyWindow(QMainWindow, form_class):
 
         self.pushButton.clicked.connect(self.btn_clicked)
 
-        # self.label = QLabel("Hello")
-        # self.label.show()
+        # QTime 설정
+        self.timer = QTimer(self)
+        self.timer.start(1000)
+        self.timer.timeout.connect(self.timeout)
 
     def btn_clicked(self):
-        print("버튼 클릭")
-        self.setGeometry(500, 300, 400, 400)
+        # print("== 코인 현재가 조회 ==")
+        price = pyupbit.get_current_price("KRW-BTC")
+
+        # price는 float형 이기때문에 str로 형변환해서 입력함.
+        self.lineEdit.setText(str(price))
+        # print(price)
+
+    def timeout(self):
+        cur_time = QTime.currentTime()
+        str_time = cur_time.toString("hh:mm:ss")
+        self.statusBar().showMessage(str_time)
 
 
 app = QApplication(sys.argv)
